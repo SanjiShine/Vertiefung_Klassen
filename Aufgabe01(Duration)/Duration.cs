9b76b913a5bf4f9cf9c49c497336be241f1def39
Sanji <chokpichit@poonpin.de>
2024-04-18 11:13:16 +0200

    Duration und Program

---
+++ Aufgabe01(Duration)/Duration.cs
@@ -0,0 +1,59 @@
+﻿using System;
+namespace Aufgabe01_Duration_
+{
+	public class Duration
+	{
+		public int days { get; set; }
+		public int hours { get; set; }
+		public int minutes { get; set; }
+
+		public Duration(int days, int hours, int minutes)
+		{
+			this.days = days;
+			this.hours = hours;
+			this.minutes = minutes;
+		}
+
+        public override string ToString()
+        {
+			return $"Es dauert: {this.days} Tage, {this.hours} Stunden und {this.minutes} Minuten.";
+        }
+
+        public static Duration operator + (Duration x, Duration y)
+        {
+            return new Duration(x.days + y.days, x.hours + y.hours, x.minutes + y.minutes);
+        }
+
+        public static Duration operator - (Duration x, Duration y)
+        {
+            return new Duration(x.days - y.days, x.hours - y.hours, x.minutes - y.minutes);
+        }
+
+        public static bool operator < (Duration x, Duration y)
+        {
+            return (int)x < (int)y;
+        }
+
+        public static bool operator > (Duration x, Duration y)
+        {
+            return (int)x > (int)y;
+        }
+
+        public static bool operator <= (Duration x, Duration y)
+        {
+            return (int)x <= (int)y;
+        }
+
+        public static bool operator >= (Duration x, Duration y)
+        {
+            return (int)x >= (int)y;
+        }
+
+        public static explicit operator int (Duration x)
+        {
+            return (x.days * 24 * 60 + x.hours * 60 + x.minutes) * 60;
+        }
+
+
+    }
+}
---
+++ Aufgabe01(Duration)/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Aufgabe01_Duration_;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Duration d1 = new Duration(1, 2, 3);
+        Console.WriteLine($"{ d1} Das ist gleich {(int)d1} Sekunden");
+
+        Duration d2 = new Duration(2, 3, 4);
+        Console.WriteLine(d1 + d2);
+        Console.WriteLine(d2 - d1);
+
+        Console.WriteLine(d1 < d2);
+        Console.WriteLine(d1 > d2);
+
+        Duration d3 = new Duration(2, 3, 4);
+
+        Console.WriteLine(d2 <= d3);
+        Console.WriteLine(d2 >= d3);
+    }
+}
+
