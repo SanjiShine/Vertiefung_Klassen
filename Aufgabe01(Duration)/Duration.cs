9b5609b8e316a6a4cea84f1ecab23441b25580ce
Sanji <chokpichit@poonpin.de>
2024-04-23 16:12:16 +0200

    Stunden und Minuten Userseingabe angepasst

--- Aufgabe01(Duration)/Duration.cs
+++ Aufgabe01(Duration)/Duration.cs
@@ -3,30 +3,43 @@ namespace Aufgabe01_Duration_
 {
 	public class Duration
 	{
-		public int days { get; set; }
-		public int hours { get; set; }
-		public int minutes { get; set; }
+		public int Days { get; set; }
+		public int Hours { get; set; }
+		public int Minutes { get; set; }
 
 		public Duration(int days, int hours, int minutes)
 		{
-			this.days = days;
-			this.hours = hours;
-			this.minutes = minutes;
+            int totalMinutes = days * 24 * 60 + hours * 60 + minutes;
+
+            this.Days = ((totalMinutes / 24) / 60);
+            this.Minutes = totalMinutes % 60;
+            this.Hours = (totalMinutes - ((this.Days * 24 * 60) + this.Minutes)) / 60;
 		}
 
         public override string ToString()
         {
-			return $"Es dauert: {this.days} Tage, {this.hours} Stunden und {this.minutes} Minuten.";
+			return $"Es dauert: {this.Days} Tage, {this.Hours} Stunden und {this.Minutes} Minuten.";
         }
 
         public static Duration operator + (Duration x, Duration y)
         {
-            return new Duration(x.days + y.days, x.hours + y.hours, x.minutes + y.minutes);
+            int totalMinutes = TimeToMinutes(x) + TimeToMinutes(y);
+
+            return MinutesToTime(totalMinutes);
         }
 
         public static Duration operator - (Duration x, Duration y)
         {
-            return new Duration(x.days - y.days, x.hours - y.hours, x.minutes - y.minutes);
+            int timeX = TimeToMinutes(x);
+            int timeY = TimeToMinutes(y);
+            
+            if(timeX > timeY) {
+
+                return MinutesToTime(timeX - timeY);
+        
+            }else {
+                return MinutesToTime(timeY - timeX);
+            }
         }
 
         public static bool operator < (Duration x, Duration y)
@@ -51,9 +64,22 @@ namespace Aufgabe01_Duration_
 
         public static explicit operator int (Duration x)
         {
-            return (x.days * 24 * 60 + x.hours * 60 + x.minutes) * 60;
+            return (x.Days * 24 * 60 + x.Hours * 60 + x.Minutes) * 60;
         }
 
+        public static int TimeToMinutes(Duration x)
+        {
+            return  x.Days * 24 * 60 + x.Hours * 60 + x.Minutes;
+        }
+
+        public static Duration MinutesToTime(int minutes)
+        {
+            int days = (minutes / 24) / 60;
+            int min = minutes % 60;
+            int hours = (minutes - ((days * 24 * 60) + min))/60;
+
+            return new Duration(days, hours, min);
+        }
 
     }
 }
--- Aufgabe01(Duration)/Program.cs
+++ Aufgabe01(Duration)/Program.cs
@@ -4,8 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        Duration d1 = new Duration(1, 2, 3);
-        Console.WriteLine($"{ d1} Das ist gleich {(int)d1} Sekunden");
+        Duration d1 = new Duration(1, 30, 50);
+        Console.WriteLine($"{d1} Das ist gleich {(int)d1} Sekunden");
 
         Duration d2 = new Duration(2, 3, 4);
         Console.WriteLine(d1 + d2);
