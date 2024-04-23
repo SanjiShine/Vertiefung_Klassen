8db8d4060189fcdce2e5eac50f372adcd625efec
Sanji <chokpichit@poonpin.de>
2024-04-23 12:06:10 +0200

    Vector Program

---
+++ Aufgabe03(Vector)/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Aufgabe03_Vector_;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Vector v1 = new Vector(1, 1);
+        Vector v2 = new Vector(1, 1);
+
+        Console.WriteLine(1.0f * (float)v1);
+        Console.WriteLine(-(v1 + v1 * 5.0f + ++v1));
+        Console.WriteLine(new Vector(1, 1) + ++v2 * 10 + v2++);
+        Console.WriteLine(v2);
+        Console.WriteLine(v1);
+        Console.WriteLine(v1 == v2 ? "ja" : "nein");
+        Console.WriteLine(new Vector(1, 1) != new Vector(1, 1) ? "ja" : "nein");
+
+
+    }
+}
+
---
+++ Aufgabe03(Vector)/Vector.cs
@@ -0,0 +1,60 @@
+﻿using System;
+namespace Aufgabe03_Vector_
+{
+	public class Vector
+	{
+		float x;
+		float y;
+
+		public Vector(float x, float y)
+		{
+			this.x = x;
+			this.y = y;
+		}
+
+		public static Vector operator + (Vector a, Vector b)
+		{
+			return new Vector(a.x + b.x, a.y + b.y);
+		}
+
+		public static Vector operator * (Vector a, float n)
+		{
+			return new Vector(a.x * n, a.y * n);
+		}
+
+		public static Vector operator ++ (Vector a)
+		{
+			a.x += a.x;
+			a.y += a.y;
+
+			return a;
+		}
+
+		public static Vector operator - (Vector a)
+		{
+			return new Vector(-a.x, -a.y);
+		}
+
+		public static bool operator == (Vector a, Vector b)
+		{
+			return a.x == b.x && a.y == b.y;
+		}
+
+        public static bool operator != (Vector a, Vector b)
+        {
+            return a.x != b.x || a.y != b.y;
+        }
+
+		public static explicit operator float(Vector a)
+		{
+			return (float)Math.Sqrt(a.x * a.x + a.y * a.y);
+
+        }
+
+		public override string ToString()
+        {
+			return $"({this.x},{this.y})";
+        }
+    }
+}
+
