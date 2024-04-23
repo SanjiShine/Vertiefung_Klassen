ef9c6bf7599efb038da77d5f87e01b98f2694bd9
Sanji <chokpichit@poonpin.de>
2024-04-23 11:35:13 +0200

    CPoint Program

---
+++ Aufgabe06(CPoint)/CPoint.cs
@@ -0,0 +1,45 @@
+﻿using System;
+namespace Aufgabe06_CPoint_
+{
+	public class CPoint
+	{
+		double x, y;
+
+		public CPoint(double x, double y)
+		{
+			this.x = x;
+			this.y = y;
+		}
+
+		public static CPoint operator * (CPoint a, int b)
+		{
+			return new CPoint(a.x * b, a.y * b);
+		}
+
+		public static CPoint operator - (CPoint a, CPoint b)
+		{
+			return new CPoint(a.x - b.x, a.y - b.y);
+		}
+
+		public static bool operator == (CPoint a, CPoint b)
+		{
+			return a.x == b.x && a.y == b.y;
+		}
+
+		public static bool operator != (CPoint a, CPoint b)
+		{
+			return a.x != b.x || a.y != b.y;
+        }
+
+		public static explicit operator double (CPoint x)
+		{ 
+			return Math.Sqrt(Math.Pow(x.x, 2) + Math.Pow(x.y, 2));	
+		}
+
+        public override string ToString()
+        {
+			return $"({x},{y})";
+        }
+    }
+}
+
---
+++ Aufgabe06(CPoint)/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Aufgabe06_CPoint_;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        CPoint p1 = new CPoint(10, 10);
+        CPoint p2 = p1 * 2; // Skalarprodukt (alle Komponenten werden mit dem Faktor multipliziert
+        CPoint p3 = p2 - p1; // Differenz zweier Vektoren
+        double d = (double)p3; // Vektorlänge (mittels Konvertierung nach double)
+        if((p2 - p1) == p1) Console.WriteLine("Punkte sind gleich!");
+        Console.WriteLine(p2 * 3 - p3);
+        Console.WriteLine(d);
+    }
+}
+
