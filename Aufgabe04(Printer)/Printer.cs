9baf7e210552c0d898aa8c596253da8499bbbc3b
Sanji <chokpichit@poonpin.de>
2024-04-30 12:12:50 +0200

    Printer and Program

---
+++ Aufgabe04(Printer)/Printer.cs
@@ -0,0 +1,54 @@
+﻿using System;
+namespace Aufgabe04_Printer_
+{
+    class Printer
+    {
+        private char printCharacter;
+        private string output = "";
+        public Printer(char printCharacter)
+        {
+            this.printCharacter = printCharacter;
+        }
+        private void PrintOneCharacter()
+        {
+            output += printCharacter;
+        }
+        public static Printer operator ++(Printer printer)
+        {
+            printer.PrintOneCharacter();
+            return printer;
+        }
+        public static Printer operator +(Printer printer, int addierteZahl)
+        {
+            for (int i = 0; i < addierteZahl; i++)
+            {
+                printer.PrintOneCharacter();
+            }
+            return printer;
+        }
+        public static Printer operator <<(Printer printer, char newChar)
+        {
+            printer.printCharacter = newChar;
+            return printer;
+        }
+        // Overload the greater-than operator (>), to compare the number of printed characters
+        public static bool operator >(Printer printer1, Printer printer2)
+        {
+            // Assumption: Number of printed characters is equal to the length of the output string
+            return printer1.output.Length > printer2.output.Length;
+        }
+        // Overload the less-than operator (<), to compare the number of printed characters
+        public static bool operator <(Printer printer1, Printer printer2)
+        {
+            // Assumption: Number of printed characters is equal to the length of the output string
+            return printer1.output.Length < printer2.output.Length;
+        }
+        // Overload the explicit cast to string
+        public static explicit operator string(Printer printer)
+        {
+            return printer.output;
+        }
+
+    }
+}
+
---
+++ Aufgabe04(Printer)/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Aufgabe04_Printer_;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Erzeugen eines #-Druckers 
+        Printer printer1 = new Printer('#');
+        // Drucken eines #-Zeichens 
+        printer1++;
+        Console.WriteLine("1. " + (string)printer1);
+        // Drucken von 3 weiteren #-Zeichen 
+        printer1 += 3;
+        Console.WriteLine("2. " + (string)printer1);
+
+        // Wechseln des Druck-Zeichens (von '#' zu 'X') und Drucken von 2 
+        //weiteren X-Zeichen
+        printer1 = printer1 << 'X';
+        printer1 = printer1 + 2;
+        Console.WriteLine("3. " + (string)printer1);
+
+        // Erzeugen eines O-Druckers und Drucken von 5 Zeichen 
+        Printer printer2 = new Printer('O') + 5;
+        Console.WriteLine("4. " + (string)printer2);
+
+        // Vergleich der zwei gedruckten Längen (= Anzahl der Zeichen in der Ausgabe) 
+        if (printer1 > printer2)
+            Console.WriteLine("5. Der 1. Drucker hat mehr Zeichen gedruckt.");
+        else
+            Console.WriteLine("5. Der 2. Drucker hat mehr Zeichen gedruckt.");
+
+    }
+}
+
